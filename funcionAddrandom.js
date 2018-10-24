51fc9a06c22124434e3c466d1a2e981cfab8b348
memolop10 <inglopezvargas@gmail.com>
2018-10-24 16:56:43 -0500

    cambio2

--- funcionAddrandom.js
+++ funcionAddrandom.js
@@ -1,8 +1,13 @@
 let vacio =[];
 
 
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  getRandomArbitrary(1,10);
+
 for(let x = 0; x<=vacio.length;x++){
 
-    vacio.push(vacio[x]);
 
 }
